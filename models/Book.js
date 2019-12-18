594e1e6e849047b2345e5d4aaa592201617cc1e9
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-18 14:35:41 -0600

    Added Db and routes

---
+++ models/Book.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+    title: {
+        type: String
+    },
+    authors: [{
+        type: String
+    }],
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    link: {
+        type: String
+    }
+});
+
+var Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
