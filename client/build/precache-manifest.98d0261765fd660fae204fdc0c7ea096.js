9dbe5295127a77dbd3e5bc884f9c5934f5c06433
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-18 18:57:14 -0600

    Ran build

---
+++ client/build/precache-manifest.98d0261765fd660fae204fdc0c7ea096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ff747afb4430f93d21e",
+    "url": "/static/js/main.3bc5c06d.chunk.js"
+  },
+  {
+    "revision": "ef3337ee7e75e1a7eb3c",
+    "url": "/static/js/2.cc366869.chunk.js"
+  },
+  {
+    "revision": "5ff747afb4430f93d21e",
+    "url": "/static/css/main.a1643905.chunk.css"
+  },
+  {
+    "revision": "6c1068c9ea42790d69f4081f291bb91e",
+    "url": "/index.html"
+  }
+];
